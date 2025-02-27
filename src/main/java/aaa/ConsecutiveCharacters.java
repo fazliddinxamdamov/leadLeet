4b82ac971e20a080695a83d66c9101d8e832a213
fazliddin <fazliddinxamdamov102@gmail.com>
2025-02-27 18:36:24 +0500

    ++

---
+++ src/main/java/aaa/ConsecutiveCharacters.java
@@ -0,0 +1,28 @@
+package aaa;
+
+public class ConsecutiveCharacters {
+
+    public int maxPower(String s) {
+
+        int count = 0;
+        int maxCount = 0;
+        char previous = ' ';
+
+        for (int i = 0; i < s.toCharArray().length; i++) {
+
+            char c = s.charAt(i);
+
+            if (c == previous) {
+                count++;
+            } else {
+                count = 1;
+                previous = c;
+            }
+
+            maxCount = Math.max(maxCount, count);
+
+        }
+
+        return maxCount;
+    }
+}
